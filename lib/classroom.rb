8a5290f4b117abb78b808b159eeb19ef86edb3b0
Amanda McGuire <aruwart001@gmail.com>
2021-05-12 14:43:46 -0600

    initial commit

--- lib/classroom.rb
+++ lib/classroom.rb
@@ -1,2 +1,9 @@
 class Classroom
+  attr_reader :subject, :capacity, :students
+
+  def initialize(subject, capacity)
+    @capacity = capacity
+    @subject = subject
+    @students = []
+  end
 end
